1945a4f25d2555685b76e7e240d5e1ec84bed20d
myname <myemail>
2019-08-05 16:04:11 +0800

    添加响应拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,27 @@
 import axios from 'axios'
 import store from '@/store'
-import router from '../router'
+import router from '@/router'
 
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 // 下面的token只能执行一次
 // axios.defaults.headers = {
 //   Authorization: `Bearer ${store.getUser().token}`
 // }
-axios.interceptors.request.use(function (config) {
-  // 在发送请求之前做些什么
-  // 修改config,追加headers
-  config.headers = {
-    Authorization: `Bearer ${store.getUser().token}`
+axios.interceptors.request.use(
+  function (config) {
+    // 在发送请求之前做些什么
+    // 修改config,追加headers
+    config.headers = {
+      Authorization: `Bearer ${store.getUser().token}`
+    }
+    return config
+  },
+  function (error) {
+    // 认为返回错误的promise对象
+    return Promise.reject(error)
   }
-  return config
-}, function (error) {
-  // 认为返回错误的promise对象
-  return Promise.reject(error)
-})
+)
+// 添加响应拦截器
 axios.interceptors.response.use(function (response) {
   // 对响应数据做点什么
   return response
